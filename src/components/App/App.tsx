c6df4615a033853ef583660fdb15c58c8f11316c
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-09-12 12:10:44 +0300

    modal

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,40 +1,52 @@
-
-import SearchBar from '../SearchBar/SearchBar'
-import './App. module.css'
+import SearchBar from '../SearchBar/SearchBar';
+import './App.module.css';
 import MovieGrid from '../Movie Grid/MovieGrid';
 import type { Movie } from '../../types/movie';
 import { useState } from "react";
 import { fetchMovies } from '../../servicies/movieService';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../Error Message/ErrorMessage';
+import MovieModal from '../Movie Modal/MovieModal';
 
 function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [isLoader, setLoader] = useState(false);
   const [error, setError] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
   const handleSelectMovie = (movie: Movie) => {
-    console.log("Вибраний фільм:", movie); // тепер використовується
+    setSelectedMovie(movie);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
   };
 
   const handleSearch = async (searchQuery: string) => {
     try {
-      setLoader(true)
-      const results = await fetchMovies(searchQuery); // searchQuery тепер визначений
+      setError(false);
+      setLoader(true);
+      const results = await fetchMovies(searchQuery);
       setMovies(results);
     } catch { 
       setError(true);
     } finally {
-      setLoader(false)
+      setLoader(false);
     }
   };
 
   return (
     <div>
       {isLoader && <Loader />}
-      {error&&<ErrorMessage /> }
+      {error && <ErrorMessage />}
       <SearchBar onSubmit={handleSearch} />
       <MovieGrid movies={movies} onSelect={handleSelectMovie} />
+      {selectedMovie && (
+        <MovieModal 
+          movie={selectedMovie} 
+          onClose={handleCloseModal} 
+        />
+      )}
     </div>
   );
 }
