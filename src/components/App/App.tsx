4eaea71a1932d4a35c70d0a16bca0f94f022be2e
Marina Prushak <87096097+huruzka@users.noreply.github.com>
2025-09-12 12:36:03 +0300

    structure correction

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import './App.module.css';
 import MovieGrid from '../Movie Grid/MovieGrid';
 import type { Movie } from '../../types/movie';
 import { useState } from "react";
-import { fetchMovies } from '../../servicies/movieService';
+import { fetchMovies } from '../../services/movieService';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../Error Message/ErrorMessage';
 import MovieModal from '../Movie Modal/MovieModal';
